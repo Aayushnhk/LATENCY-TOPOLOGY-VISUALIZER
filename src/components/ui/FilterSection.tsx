fcb379e3a331cc61c8b46b1bcd09666ab769d818
Aayushnhk <asaayush019@gmail.com>
2025-11-11 18:22:13 +0530

    Filter updated

--- src/components/ui/FilterSection.tsx
+++ src/components/ui/FilterSection.tsx
@@ -58,7 +58,7 @@ export function FilterSection({ exchanges, regions }: FilterSectionProps) {
                             checked={showConnections}
                             onChange={toggleConnections} 
                         />
-                        {/* Enhanced Toggle Switch Style */}
+                        {/* Toggle Switch Style */}
                         <div className="w-12 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-cyan-500 shadow-inner cursor-pointer"></div>
                     </label>
                 </div>
